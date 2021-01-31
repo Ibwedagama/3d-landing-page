09eca1455775b794dad14c767747e7fb58b7cebe
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-01-31 14:32:26 +0800

    [ibw] initial commit

---
+++ public/animation.js
@@ -0,0 +1,15 @@
+gsap.from('.animate', {
+	opacity: 0,
+	duration: 0.9,
+	delay: 1.3,
+	y: -50,
+	ease: 'Power4.InOut',
+	stagger: 0.3,
+})
+
+gsap.from('.illustration', {
+	opacity: 0,
+	duration: 1.5,
+	delay: 2.5,
+	ease: 'Power.InOut',
+})
